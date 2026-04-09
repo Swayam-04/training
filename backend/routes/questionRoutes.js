9952d44742fca436687d59f4f75d83a564b4ccb7
Swayam Barik <79577806+Swayam-04@users.noreply.github.com>
2026-04-09 11:03:57 +0530

    Delete backend directory

--- backend/routes/questionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const questionController = require('../controllers/questionController');
-
-router.post('/', questionController.addQuestion);
-router.get('/:moduleId', questionController.getQuestionsByModule); // Note: using moduleId as param based on controller logic
-router.delete('/:id', questionController.deleteQuestion);
-
-module.exports = router;
